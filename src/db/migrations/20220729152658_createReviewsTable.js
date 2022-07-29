5a7349cb0a02f9e11fec3e9f373489bfb3790a56
JB Burroughs <jbburroughs.dev@gmail.com>
2022-07-29 15:51:58 -0400

    fixed all FKs for migration, all tables successfully migrated

--- src/db/migrations/20220729152658_createReviewsTable.js
+++ src/db/migrations/20220729152658_createReviewsTable.js
@@ -4,20 +4,22 @@ exports.up = function(knex) {
     table.increments("review_id").primary();
     table.text("content");
     table.integer("score");
+    table.integer("movie_id").unsigned().notNullable();
     table
         .foreign("movie_id")
         .references("movie_id")
         .inTable("movies")
-        .onDelete("cascade");
+        .onDelete("CASCADE");
+    table.integer("critic_id").unsigned().notNullable();
     table
         .foreign("critic_id")
         .references("critic_id")
         .inTable("critics")
-        .onDelete("cascade");
+        .onDelete("CASCADE");
     table.timestamps(true, true)
   })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("reviews")
 };
