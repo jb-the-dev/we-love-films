e588d4338b96d07fbb18e4c069e9a08a920adb4f
JB Burroughs <jbburroughs.dev@gmail.com>
2022-08-04 11:06:34 -0400

    Refactor app.js errors, add cors, homepage router

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+const homeRouter = express.Router();
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
 const theatersRouter = require("./theaters/theaters.router")
 
+const notFound = require("./errors/notFound")
+const errorHandler = require("./errors/errorHandler")
+
+app.use(cors())
 app.use(express.json());
 
+// CUSTOM HOMEPAGE ROUTE
+homeRouter.get('/', (req, res) => {
+  res.json({ message:
+  'Welcome! You can access data via the following routes: /movies, /reviews, /theaters, /reviews/:reviewId, /movies/:movieId, /movies/:movieId/theaters, /movies/:movieId/reviews.'});
+})
+
+app.use('/', homeRouter);
+
+// ROUTERS
 app.use("/movies", moviesRouter)
 app.use("/reviews", reviewsRouter)
 app.use("/theaters", theatersRouter)
 
-// Not found handler
-app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
-
-// Error handler
-app.use((error, req, res, next) => {
-  // console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
-});
+// ERRORS
+app.use(notFound)
+app.use(errorHandler)
 
 module.exports = app;
